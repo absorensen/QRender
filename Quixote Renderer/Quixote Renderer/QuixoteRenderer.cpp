cd0f2c3239fef06a3ddd366e5fd37361f253541f
andersS91 <anders-soerensen@hotmail.com>
2018-06-12 07:22:48 +0200

    Basic light on

--- Quixote Renderer/Quixote Renderer/QuixoteRenderer.cpp
+++ Quixote Renderer/Quixote Renderer/QuixoteRenderer.cpp
@@ -18,6 +18,9 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+// lights
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 
 int main()
 {
@@ -53,6 +56,7 @@ int main()
 
 	glEnable(GL_DEPTH_TEST);
 
+	Shader lightingShader("simpleLight.vs", "simpleLight.fs");
 	Shader ourShader("shader.vs", "shader.fs");
 
 	float vertices[] = {
@@ -122,7 +126,7 @@ int main()
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	//glGenBuffers(1, &EBO);
 
 	// bind Vertex Array Object
 	glBindVertexArray(VAO);
@@ -132,19 +136,16 @@ int main()
 	// copy data to buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
 
 
 	// load and create a texture
-	unsigned int texture1, texture2;
+	/*unsigned int texture1, texture2;
 	glGenTextures(1, &texture1);
 	glBindTexture(GL_TEXTURE_2D, texture1);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -155,12 +156,12 @@ int main()
 	int width, height, nrChannels;
 	stbi_set_flip_vertically_on_load(true);
 	unsigned char *data = stbi_load("Resources/Textures/container.jpg", &width, &height, &nrChannels, 0);
-	if (data) 
+	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
-	else 
+	else
 	{
 		std::cout << "Failed to load texture" << std::endl;
 	}
@@ -171,7 +172,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
+
 	data = stbi_load("Resources/Textures/awesomeface.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
@@ -182,15 +183,22 @@ int main()
 	{
 		std::cout << "Failed to load texture" << std::endl;
 	}
-	stbi_image_free(data);
-	
-
-	ourShader.use(); 
-	ourShader.setInt("texture1", 0);
-	ourShader.setInt("texture2", 1);
+	stbi_image_free(data);*/
 
 
-	
+	//ourShader.use(); 
+	//ourShader.setInt("texture1", 0);
+	//ourShader.setInt("texture2", 1);
+
+
+	unsigned int lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
 
 	// while the window has not been closed
 	while (!glfwWindowShouldClose(window)) {
@@ -204,31 +212,63 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
-
-		ourShader.use();
+		// LIGHTS
+		lightingShader.use();
+		glm::mat4 model = glm::mat4();
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f));
+		lightingShader.setMat4("model", model);
 
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), ASPECT_RATIO, 0.1f, 100.0f);
-		ourShader.setMat4("projection", projection);
+		lightingShader.setMat4("projection", projection);
 
 		glm::mat4 view = camera.GetViewMatrix();
+		lightingShader.setMat4("view", view);
+
+		glBindVertexArray(lightVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+		// BOXES
+	/*	glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);*/
+
+		ourShader.use();
+		ourShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+		ourShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+
+		projection = glm::perspective(glm::radians(camera.Zoom), ASPECT_RATIO, 0.1f, 100.0f);
+		ourShader.setMat4("projection", projection);
+
+		view = camera.GetViewMatrix();
 		ourShader.setMat4("view", view);
 
 		glBindVertexArray(VAO);
 
-		for (unsigned int i = 0; i < 10; ++i) {
-			glm::mat4 model;
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i ;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			ourShader.setMat4("model", model);
+		model = glm::mat4();
+		model = glm::translate(model, cubePositions[0]);
+		float angle = 20.0f;
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		ourShader.setMat4("model", model);
 
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
+
+		//for (unsigned int i = 0; i < 10; ++i) {
+		//	model = glm::mat4();
+		//	model = glm::translate(model, cubePositions[i]);
+		//	float angle = 20.0f * i ;
+		//	model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		//	ourShader.setMat4("model", model);
+
+		//	glDrawArrays(GL_TRIANGLES, 0, 36);
+		//}
+
+
+
+		// END
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
@@ -238,7 +278,7 @@ int main()
 	// de-allocate resources
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	//glDeleteBuffers(1, &EBO);
 
 	// deallocate
 	glfwTerminate();
@@ -272,7 +312,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 		lastY = ypos;
 		firstMouse = false;
 	}
-	
+
 	float xoffset = xpos - lastX;
 	float yoffset = lastY - ypos;
 	lastX = xpos;
